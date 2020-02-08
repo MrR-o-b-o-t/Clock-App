a519ec63761913f4220c9436c7b50cb35deaae2d
Sam Kautz <samkautz@yahoo.com>
2020-02-07 21:02:14 -0600

    Updated code to reflect OOP

--- scripts.js
+++ scripts.js
@@ -2,69 +2,81 @@
 
 let clock = setInterval(displayTime, 0);
 
-function getBaseTime() {
-    let hms = new Date();
-    return hms.toLocaleTimeString();
-}
-
-/* DISPLAYS MILITARY TIME */
-function getMilitaryTime() {
-    let militaryHours = new Date();
-    let newMilitaryHours = militaryHours.getHours();
-
-    let militaryMinutes = new Date();
-    let newMilitaryMinutes = militaryMinutes.getMinutes();
-
-    let militarySeconds = new Date();
-    let newMilitarySeconds = militarySeconds.getSeconds();
-
-    return newMilitaryHours + ":" + newMilitaryMinutes + ":" + newMilitarySeconds;
-}
-
-
 /* ALLOWS MILITARY AND STANDARD TIME TO BE TOGGLED */
 function displayTime() {
-    if (document.querySelector("#military-toggler input:checked")) {
-        document.getElementById("time").innerHTML = getMilitaryTime()
-    }
-    else {
-        document.getElementById("time").innerHTML = getBaseTime()
-    }
+  const isMilitaryTime = document.querySelector(
+    "#military-toggler input:checked"
+  );
+
+  const currentDate = new Date();
+
+  const hours = isMilitaryTime ? currentDate.getHours() : getStandardHours(currentDate);
+  const minutes = formatTime(currentDate.getMinutes());
+  const seconds = formatTime(currentDate.getSeconds());
+  const amPm = isMilitaryTime ? "" : getAmPm(currentDate);
+
+  document.getElementById("time").innerHTML = `${hours}:${minutes}:${seconds} ${amPm}`;
+
+  /* DISPLAYS DAY OF THE WEEK IN TEXT, MONTH, DATE, AND YEAR. */
+  document.getElementById("weekday").innerHTML = getDay(currentDate) + " - ";
+  document.getElementById("month").innerHTML = getMonth(currentDate);
+  document.getElementById("date").innerHTML = currentDate.getDate() + ",";
+  document.getElementById("year").innerHTML = currentDate.getFullYear();
 }
 
+function getAmPm(date) {
+  if (date.getHours() < 12) {
+    return "AM";
+  }
+  return "PM";
+}
 
-/* DISPLAYS DAY OF THE WEEK IN TEXT, MONTH, DATE, AND YEAR. */
-let setWeekday = new Date();
-let weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+function getStandardHours(date) {
+  const hours = date.getHours();
+  if (hours > 12) {
+    return hours - 12;
+  }
+  if (hours === 0) {
+    return 12;
+  }
+  return hours;
+}
 
-let n = weekday[setWeekday.getDay()];
-document.getElementById("weekday").innerHTML = n + " - "
+function getDay(date) {
+  let weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
 
-let d = new Date();
-let months =
-    ["January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"];
-document.getElementById("month").innerHTML = months[d.getMonth()];
+  return weekday[date.getDay()];
+}
 
-let getTodaysDate = new Date();
-document.getElementById("date").innerHTML = getTodaysDate.getDate() + ",";
+function getMonth(date) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
-let getFullYear = new Date();
-document.getElementById("year").innerHTML = getFullYear.getFullYear();
+  return months[date.getMonth()];
+}
+
+function formatTime(time) {
+  if (time < 10) {
+    return "0" + time;
+  }
+  return time;
+}
